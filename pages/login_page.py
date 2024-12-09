356c8228dce43144ce5b944ccdb130a225168107
anohin <nvanohin@vtb.ru>
2024-12-09 15:30:08 +0300

    fixed assert in should_be_login_url

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert self.url == LoginPageLocators.LOGIN_URL, \
+        assert self.browser.current_url == LoginPageLocators.LOGIN_URL, \
             "This is page not login page"
 
     def should_be_login_form(self):
