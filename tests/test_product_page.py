62b402c98ea7126b0cda7e7a3879806f559847d9
anohin <nvanohin@vtb.ru>
2024-12-10 13:28:19 +0300

    added two tests in test_product_page

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,5 +1,8 @@
+from itertools import product
+
 import pytest
 
+from conftest import browser
 from pages.product_page import ProductPage
 
 
@@ -36,3 +39,17 @@ def test_guest_cant_see_success_message():
 
 def test_message_disappeared_after_adding_product_to_basket():
     assert False, "Test not implemented"
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.go_to_login_page()
